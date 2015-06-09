ff3467b18eaa44fe633616579a8cebea9a35db1c
Ondrej Mikle <ondrej.mikle@nic.cz>
2015-06-09 10:59:52 +0200

    Telnet terminal redirects to interactive shell

--- kippo/core/telnet.py
+++ kippo/core/telnet.py
@@ -1,27 +1,44 @@
 from twisted.internet import protocol
 from twisted.manhole import telnet
 from twisted.conch import recvline
-from twisted.conch.telnet import TelnetTransport, TelnetBootstrapProtocol
+from twisted.conch.telnet import TelnetTransport, TelnetBootstrapProtocol, TelnetProtocol
 from twisted.conch.insults import insults
 
-class DemoRecvLine(recvline.HistoricRecvLine):
+from kippo.core.protocol import HoneyPotInteractiveProtocol, LoggingServerProtocol
+from kippo.core.ssh import HoneyPotAvatar
+from kippo.core.honeypot import HoneyPotEnvironment, HoneyPotShell
+
+class TelnetShell(recvline.HistoricRecvLine):
     """Simple echo protocol.
 
     Accepts lines of input and writes them back to its connection.  If
     a line consisting solely of \"quit\" is received, the connection
     is dropped.
     """
+    
+    def __init__(self, *args, **kwargs):
+        env = HoneyPotEnvironment()
+        user = HoneyPotAvatar("root", env)
+        
+        self.honeypot = HoneyPotInteractiveProtocol(user, env)
+        self.shell = None
+        pass
 
     def lineReceived(self, line):
-        if line == "quit":
-            self.terminal.loseConnection()
-        self.terminal.write(line)
-        self.terminal.nextLine()
-        self.terminal.write(self.ps[self.pn])
+        if not self.shell:
+            self.honeypot.terminal = self.terminal
+            self.shell = HoneyPotShell(self.honeypot)
+        
+        self.shell.lineReceived(line)
 
 class HoneyPotTelnetFactory(protocol.ServerFactory):
     def __init__(self):
         self.protocol = lambda: TelnetTransport(TelnetBootstrapProtocol,
                                              insults.ServerProtocol,
-                                             DemoRecvLine)
+                                             TelnetShell,
+                                             #telnetShellArg,
+                                             #telnetShellKWArg="zxcv"
+                                             )
 
+#env = HoneyPotEnvironment()
+#user = HoneyPotAvatar("root", env)
